e1933bc48b2fcfb230d3629588498526d6bae4c7
TajwarSaiyeed <rjabid36@gmail.com>
2022-12-17 13:26:55 +0600

    services page update

--- src/pages/Services/ServicesSectionOne.jsx
+++ src/pages/Services/ServicesSectionOne.jsx
@@ -4,41 +4,66 @@ const ServicesSectionOne = () => {
   const grhDetails = [
     {
       id: 1,
-      title: "Research",
+      title: "Built SEO",
       description:
-        "HMS specialises in developing innovative, efficient and smart healthcare solutions.",
+        "SEO Brings Higher patient retention Rates which will Results into Higher Conversion Rate.",
+      image: "https://i.ibb.co/hYDPXrd/seo.png",
     },
     {
       id: 2,
-      title: "HMS Customization",
+      title: "Hospital Profile",
       description:
-        "We offer complete HMS customization solutions. We are staffed by eLearning experts and we know how to get the most from HMS.",
+        "More than 80% of people searching for medical professionals make their selection from HMS.",
+      image: "https://i.ibb.co/7tJ1bmH/hosp.png",
     },
     {
       id: 3,
-      title: "Cost Effective",
+      title: "Online Appointment",
       description:
-        "HMS not only saves time in the hospital but also is cost-effective in decreasing the number of people working on the Paper work.",
+        "Provide comfort to your patients in this pandemic situation to book online appointments.",
+      image: "https://i.ibb.co/10GP36y/appointment.png",
+    },
+    {
+      id: 4,
+      title: "Articles",
+      description:
+        "Keep updated with latest techniques/knowledge/research to build a professional network.",
+      image: "https://i.ibb.co/CWdTF4s/articles.png",
+    },
+    {
+      id: 5,
+      title: "Easy to Use",
+      description:
+        "Top quality Software with all Features easy to use and easily accessible.",
+      image: "https://i.ibb.co/FbhBVbp/finger.png",
+    },
+    {
+      id: 6,
+      title: "24*7 Support",
+      description: "Any time we are here to help you.",
+      image: "https://i.ibb.co/j6b0Pw0/call.png",
     },
   ];
 
   return (
-    <div className="relative h-[800px] md:h-[800px] lg:h-[500px] mt-5 flex justify-center">
+    <div className="relative h-[800px] md:h-[800px] lg:min-h-[500px] mt-5 flex justify-center">
       <div className="bg-[#F5F5F5] w-full rounded-[50px] h-[300px] lg:h-64 shadow-md"></div>
       <div className="absolute top-10 mx-auto gap-5 flex justify-center items-center flex-col">
         <h1 className="text-3xl sm:text-6xl font-bold text-[#0011AD] mb-3">
           Grow Your Hospital
         </h1>
         <p>We Help To Grow Your Hospital Beyond Your Expectation</p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 justify-items-center">
-          {grhDetails.map((grhDetail) => (
+        <div className="grid grid-cols-3 gap-5 justify-items-center">
+          {grhDetails.map((about) => (
             <div
-              key={grhDetail.id}
-              className="text-center min-w-40 w-full md:w-72 lg:w-80 text-white flex justify-center items-center flex-col bg-[#0011AD] p-4 rounded-2xl"
+              key={about.id}
+              className="gap-4 h-52 min-w-40 w-full flex flex-col md:w-72 lg:w-80 bg-[#0011AD] shadow-lg text-white p-4 rounded-2xl"
             >
-              <h1 className="text-6xl font-bold">{grhDetail.id}</h1>
-              <h2 className="text-4xl">{grhDetail.title}</h2>
-              <p className="text-lg">{grhDetail.description}</p>
+              <img className="w-10 h-12" src={about.image} alt="" />
+              <div>
+                <h2 className="text-2xl font-bold text-white">{about.title}</h2>
+                <p className="text-lg">{about.description}</p>
+              </div>
             </div>
           ))}
         </div>
