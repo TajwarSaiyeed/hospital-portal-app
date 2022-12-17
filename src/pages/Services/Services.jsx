edd6f7755eec9ae5b900d8bb8813deea1a4fce23
TajwarSaiyeed <rjabid36@gmail.com>
2022-12-17 13:12:12 +0600

    services page update

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeAboutService from "../../components/HomeAboutService/HomeAboutService";
 import Subscribe from "../shared/Subscribe/Subscribe";
+import ServicesSectionOne from "./ServicesSectionOne";
 
 const Services = () => {
   return (
     <div>
+      <ServicesSectionOne />
       <HomeAboutService />
       <Subscribe />
     </div>
