ab6788192c993dbb943954b7685485f2b9c18a35
TajwarSaiyeed <rjabid36@gmail.com>
2022-12-17 13:13:33 +0600

    services page update

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import HomeAboutService from "../../components/HomeAboutService/HomeAboutService";
 import Subscribe from "../shared/Subscribe/Subscribe";
 import ServicesSectionOne from "./ServicesSectionOne";
+import serviceImage from "../../assets/services.png";
 
 const Services = () => {
   return (
     <div>
+      <div className="h-96 w-full">
+        <img className="w-full object-cover h-full" src={serviceImage} alt="" />
+      </div>
       <ServicesSectionOne />
       <HomeAboutService />
       <Subscribe />
