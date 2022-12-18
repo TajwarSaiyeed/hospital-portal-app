7f0700799c7365b54970c43e5924939c9fbb7c5a
TajwarSaiyeed <rjabid36@gmail.com>
2022-12-18 19:03:11 +0600

    update

--- src/pages/Home/HospitalSaas.jsx
+++ src/pages/Home/HospitalSaas.jsx
@@ -5,9 +5,9 @@ import { BsFillCheckSquareFill } from "react-icons/bs";
 
 const HospitalSaas = () => {
   return (
-    <div className="flex relative items-center">
-      <div className="hidden md:block w-1/2">
-        <img src={home_saas} alt="" />
+    <div className="flex h-[600px] sm:h-[500px] md:h-[600px] relative items-center">
+      <div className="hidden md:block w-full h-full lg:w-1/2">
+        <img className="w-full h-full object-cover" src={home_saas} alt="" />
       </div>
       <div
         style={{
@@ -15,34 +15,34 @@ const HospitalSaas = () => {
         }}
         className="w-full h-full absolute"
       ></div>
-      <div className="w-1/2 absolute top-0 right-0 h-full flex justify-center items-center flex-col p-5 gap-2">
-        <h1 className="text-[55px] text-[#0011AD] font-bold">
+      <div className="w-full lg:w-1/2 absolute top-0 right-0 h-full flex justify-center items-center flex-col p-5 gap-2">
+        <h1 className="text-2xl sm:text-4xl text-[#0011AD] font-bold">
           Why Hospital SAAS?
         </h1>
-        <p className="text-[18px] font-medium text-justify">
+        <p className="text-[18px] md:text-3xl lg:text-[18px] md:text-center lg:text-justify font-medium text-justify">
           We have implemented, Hospital SAAS for our patient's registration,
           appointment scheduling, inpatient management, ICU management, OT
           management, pharmacy.
         </p>
         <div className="flex w-full gap-2 justify-around items-center">
-          <div>
-            <p className="flex items-center gap-1 text-lg font-medium">
+          <div className="my-2">
+            <p className="flex items-center gap-1 text-sm sm:text-lg font-medium">
               <BsFillCheckSquareFill fontSize={21} />
               Fully Secure
             </p>
             <br />
-            <p className="flex items-center gap-1 text-lg font-medium">
+            <p className="flex items-center gap-1 text-sm sm:text-lg font-medium">
               <BsFillCheckSquareFill fontSize={21} />
               Easy To Use
             </p>
           </div>
           <div>
-            <p className="flex items-center gap-1 text-lg font-medium">
+            <p className="flex items-center gap-1 text-sm sm:text-lg font-medium">
               <BsFillCheckSquareFill fontSize={21} />
               Regular Updates
             </p>
             <br />
-            <p className="flex items-center gap-1 text-lg font-medium">
+            <p className="flex items-center gap-1 text-sm sm:text-lg font-medium">
               <BsFillCheckSquareFill fontSize={21} />
               24*7 Support
             </p>
--- src/pages/Home/ProtectYoutHealth.jsx
+++ src/pages/Home/ProtectYoutHealth.jsx
@@ -25,7 +25,7 @@ const ProtectYoutHealth = () => {
     },
   ];
   return (
-    <div className="relative h-[1400px] md:h-[800px] lg:h-[500px] mt-5 flex justify-center">
+    <div className="relative h-[1000px] sm:h-[700px] md:h-[800px] lg:h-[500px] mt-5 flex justify-center">
       <div className="bg-[#F5F5F5] w-full rounded-[50px] h-[300px] lg:h-64 shadow-md"></div>
       <div className="absolute top-10 mx-auto gap-5 flex justify-center items-center flex-col">
         <h1 className="text-3xl text-center sm:text-6xl font-bold text-[#0011AD] mb-3">
