67e99a55ca342b14b9944163ef6d84deffc7e750
TajwarSaiyeed <rjabid36@gmail.com>
2022-12-18 19:14:50 +0600

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
       <div className="h-[500px] md:min-h-96 w-full relative">
         <img className="w-full object-cover h-full" src={homeImage} alt="" />
         <div className="absolute flex flex-col gap-5 p-4 md:pl-10 top-2/4 -translate-y-1/2 left-0 w-full md:w-1/2">
-          <h1 className="text-2xl sm:text-4xl font-bold md:text-4xl lg:text-6xl text-[#0011AD]">
+          <h1 className="text-2xl sm:text-4xl font-bold lg:font-normal md:text-4xl lg:text-6xl text-[#0011AD]">
             Manage Hospital Never Before
           </h1>
           <p className="text-md sm:text-2xl font-semibold text-justify text-white">
