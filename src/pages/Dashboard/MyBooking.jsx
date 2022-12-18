2af3418942a45618b003a6045d28fa84ce834ec7
TajwarSaiyeed <rjabid36@gmail.com>
2022-12-18 13:16:15 +0600

    update

--- src/pages/Dashboard/MyBooking.jsx
+++ src/pages/Dashboard/MyBooking.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
+import Loading from "../../components/Loading/Loading";
 
 const MyBooking = () => {
   const { user } = useContext(AuthContext);
@@ -28,8 +29,51 @@ const MyBooking = () => {
     },
   });
 
-  console.log(myBookings);
-  return <div>My booking</div>;
+  if (isLoading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Plan Name</th>
+              <th>Price</th>
+              <th>Monthly/Yearly</th>
+              <th>Payment Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myBookings.map((myBooking, i) => (
+              <tr key={myBooking._id}>
+                <th>{i + 1}</th>
+                <td>
+                  <p className="badge badge-primary font-bold">
+                    {myBooking.planName}
+                  </p>
+                </td>
+                <td>${myBooking.price}</td>
+                <td>
+                  <p className="badge badge-lg font-bold">
+                    {myBooking.monthlyOrYearly}
+                  </p>
+                </td>
+                <td>
+                  {myBooking?.paid === true ? (
+                    "Paid"
+                  ) : (
+                    <button className="btn btn-success btn-sm">Pay</button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default MyBooking;
